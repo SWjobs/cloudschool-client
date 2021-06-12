c98b9a2d8e2c0e64cfbe97fa0adf812bbc5c8640
Hyeon Gyu Jeon <hyeongyu425@gmail.com>
2021-06-12 15:49:08 +0900

    슬로건 수정, class page생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,15 @@ import { Container } from 'react-bootstrap'
 const IndexPage = () => (
   <Layout>
     <div className="bg-primary" style={{ height: 500 }}>
-      <Container fluid="sm" className="d-flex justify-content-between align-items-center h-100">
-        <h1 className="text-white" style={{ fontSize: 60 }}>CloudSchool</h1>
-        <img src="/assets/illust.svg" style={{ width: 400 }} />
+      <Container fluid="sm" className="d-flex align-items-center h-100">
+        <h1 className="text-white" style={{ fontSize: 70 }}>CloudSchool</h1>
       </Container>
     </div>
-    <div className="bg-white" style={{ height: 500 }}>
-      
+
+  <div className="bg-white" style={{ height: 500 }}>
+      <Container fluid="sm" className="d-flex align-items-center h-100">
+        <h4 className="sm" style={{ fontSize: 50 }}>Everyday, Anywhere, for Eㄴveryone.</h4>
+      </Container>
     </div>
   </Layout>
 )
--- pages/register.tsx
+++ pages/register.tsx
@@ -5,12 +5,11 @@ import { Container, Col, Form, Row } from "react-bootstrap";
 
 const Register: NextPage = () => {
   return <Layout>
-    <Container fluid="sm" className="mt-4" style={{ maxWidth: 900 }}>
-      <h3 className="py-5">CloudSchool 회원 가입</h3>
+    <Container fluid="sm" className="mt-5" style={{ maxWidth: 900 }}>
+      <h3 className="mb-5">CloudSchool 회원 가입</h3>
       <Form noValidate>
-        <Form.Text className="font-weight-bold pb-2">* 표시는 필수 항목</Form.Text>
         <Row>
-          <Form.Label column xs={12} sm={3} md={2}>*아이디:</Form.Label>
+          <Form.Label column xs={12} sm={3} md={2}>아이디:</Form.Label>
           <Col>
             <Form.Group>
               <Form.Control type="text" placeholder="Awesome123" />
@@ -18,16 +17,15 @@ const Register: NextPage = () => {
           </Col>
         </Row>
         <Row>
-          <Form.Label column xs={12} sm={3} md={2}>*패스워드:</Form.Label>
+          <Form.Label column xs={12} sm={3} md={2}>패스워드:</Form.Label>
           <Col>
             <Form.Group>
               <Form.Control type="password" />
             </Form.Group>
           </Col>
-        </Row>
-
-        <Row className="mt-3">
-          <Form.Label column xs={12} sm={3} md={2}>*이름:</Form.Label>
+        </Row> 
+        <Row>
+          <Form.Label column xs={12} sm={3} md={2}>학교:</Form.Label>
           <Col>
             <Form.Group>
               <Form.Control type="text" />
@@ -35,6 +33,7 @@ const Register: NextPage = () => {
           </Col>
         </Row>
       </Form>
+  
     </Container>
   </Layout>
 }
